9c2f294c43121b09e610170ea9c4aee5ff6aa8dc
Diyar <diyaryvzz@gmail.com>
2023-11-29 12:38:27 +0300

    urunEkleme

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css'
-
+import UrunEkle from './urunEkle';
 function App() {
   return (
     <div className="App">
-      
+      <UrunEkle/>
     </div>
   );
 }
