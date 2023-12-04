9e3f14e7a828de9cbe1ce185e43f7a45db61176d
WebAesthete <emircankeserci65@outlook.com>
2023-12-04 19:38:26 +0300

    Formik Eklendi

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,26 +1,62 @@
-import React, { useState } from 'react';
-import '../style/Login.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import {basicSchema  } from "../schemas";
+import "../style/Login.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+const onSubmit = async (values, actions) => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 1000); //bu kısım anlaşılmadı
+  });
+  actions.resetForm();
+};
 function Login() {
+  const { values, errors, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      
+    },
+    validationSchema: basicSchema,
+    onSubmit,
+  });
   return (
     <div>
-      <form>
-        <div className='container'>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
-        </div>
-        
-        <button type="submit" className="btn btn-primary">Giriş Yap</button>
+      <form onSubmit={handleSubmit}>
+        <div className="container">
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              aria-describedby="emailHelp"
+              placeholder="Email giriniz"
+              value={values.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Şİfre giriniz"
+              value={values.password}
+              onChange={handleChange}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Giriş Yap
+          </button>
         </div>
       </form>
-      
     </div>
-    
   );
 }
 
