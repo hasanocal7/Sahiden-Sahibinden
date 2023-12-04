12bbe715ab3f077fdff970af806842a7a541d761
Diyar <diyaryvzz@gmail.com>
2023-12-04 12:56:12 +0300

    Fazla dosyalar silindi importlar eklendi

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,7 @@ import './style/Login.css'
 import UrunEkle from './urunEkle';
 //import Login from './src/Login';
 
-
 import './App.css'
-import Login from '../src/Login';
 function App() {
   return (
     <div className="App">
