cea7a450915487a6e513fc06515f4ea52cd100bc
Diyar <diyaryvzz@gmail.com>
2023-12-07 12:07:17 +0300

    Navbar yenilendi

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,16 @@ import Navbar from "./components/Navbar";
 import Ilan from "./components/Ilan";
 import UrunEkle from "./components/UrunEkle";
 import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <div className="App">
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/ilan" element={<Ilan />} />
         <Route path="/ilanyukle" element={<UrunEkle />} />
         
