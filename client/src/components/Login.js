3aecb72adda9175aba3c30106bc1412354ac7f6c
AysuMUT <141619151+AysuMUT@users.noreply.github.com>
2023-12-11 16:56:08 +0300

    hata düzeltildi

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { useFormik } from 'formik';
-import { basicSchema } from '../schemas';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../style/Login.css';
+import React from "react";
+import { useFormik } from "formik";
+import { basicSchema } from "../schemas";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../style/Login.css";
 
 const onSubmit = async (values, actions) => {
   await new Promise((resolve) => {
@@ -14,25 +14,28 @@ const onSubmit = async (values, actions) => {
 function Login() {
   const { values, errors, handleChange, handleSubmit } = useFormik({
     initialValues: {
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     },
     validationSchema: basicSchema,
     onSubmit,
   });
 
   return (
-    <div className="container mt-5 d-flex flex-column align-items-center" style={{ height: "70vh" }}>
-      <h1 className='form-title mb-4'>Giriş Yap</h1>
+    <div
+      className="loginContainer container mt-5 d-flex flex-column align-items-center"
+      style={{ height: "70vh" }}
+    >
+      <h1 className="form-title mb-4">Giriş Yap</h1>
       <form onSubmit={handleSubmit} className="w-50">
-        <div className="inputs mb-3">
+        <div className="loginİnputs mb-3">
           <label htmlFor="email" className="form-label">
-            E-Posta
+           
           </label>
           <input
             type="email"
             id="email"
-            className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+            className={`form-control ${errors.email ? "is-invalid" : ""}`}
             placeholder="E-posta giriniz"
             value={values.email}
             onChange={handleChange}
@@ -40,20 +43,22 @@ function Login() {
           {errors.email && <p className="invalid-feedback">{errors.email}</p>}
 
           <label htmlFor="password" className="form-label">
-            Password
+           
           </label>
           <input
             type="password"
             id="password"
-            className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+            className={`form-control ${errors.password ? "is-invalid" : ""}`}
             placeholder="Şifre giriniz"
             value={values.password}
             onChange={handleChange}
           />
-          {errors.password && <p className="invalid-feedback">{errors.password}</p>}
+          {errors.password && (
+            <p className="invalid-feedback">{errors.password}</p>
+          )}
         </div>
 
-        <button type="submit" className="btn btn-primary">
+        <button type="submit" className="loginButton btn ">
           Giriş Yap
         </button>
       </form>
