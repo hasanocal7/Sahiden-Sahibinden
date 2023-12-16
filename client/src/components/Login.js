e61c91d4182792d1925991f3344c3cf44b2dcd5c
AysuMUT <141619151+AysuMUT@users.noreply.github.com>
2023-12-16 17:43:01 +0300

    Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -23,7 +23,7 @@ function Login() {
 
   return (
     <div className="loginContainer container mt-5 d-flex flex-column align-items-center">
-      <h1 className="form-title mb-4">Giriş Yap</h1>
+      <h1 className="form-title mb-4">Sahiden</h1>
       <form onSubmit={handleSubmit} className="w-50">
         <div className="loginInputs mb-3">
           <label htmlFor="email" className="form-label"></label>
@@ -52,15 +52,22 @@ function Login() {
         </div>
 
         
-        <div className="d-flex justify-content-between align-items-center mb-3">
+        <div className="forgotPasswordLink">
           <a href="/forgot-password" className="forgotPasswordLink">
             Şifremi Unuttum
           </a>
-
+          </div>
+          
+          <div>
           <button type="submit" className="loginButton btn">
             Giriş Yap
           </button>
         </div>
+
+        <div className="signupLink">
+          <p>Hesabın yok mu? <a href="/signup">Hemen kayıt ol!</a></p>
+        </div>
+
       </form>
     </div>
   );
