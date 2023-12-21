616fac551e514bf3c193c03e187ccdce7acacfa3
hasan <hasanocal7@gmail.com>
2023-12-22 01:21:17 +0300

    Backend | Deployment / Trial 3

--- server/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-const authController = require("../controller/authController");
-const router = require("express").Router();
-const userAuth = require("../middleware/userAuth");
-
-router.get("/", authController.getAllUsers);
-router.get("/:id", authController.getUser);
-router.post("/signup", userAuth.beforeRegister, authController.createUser);
-
-module.exports = router;
