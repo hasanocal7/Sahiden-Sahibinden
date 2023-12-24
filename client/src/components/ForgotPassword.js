4c5ea3c576df860fc037fd8aea352f4e2aaa759d
AysuMUT <141619151+AysuMUT@users.noreply.github.com>
2023-12-24 16:56:36 +0300

    fogot password

--- client/src/components/ForgotPassword.js
+++ client/src/components/ForgotPassword.js
@@ -1,40 +1,41 @@
-import React, { useState } from 'react';
-import '../style/ForgotPassword.css';
+import React, { useState } from "react";
+import "../style/ForgotPassword.css";
 
 function ForgotPassword() {
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleForgotPassword = async () => {
     try {
-      const response = await fetch('/api/forgot-password', {
-        method: 'POST',
+      const response = await fetch("/api/forgot-password", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ email }),
       });
 
       if (response.ok) {
-        setMessage('Şifre sıfırlama bağlantısı e-posta adresinize gönderildi.');
+        setMessage("Şifre sıfırlama bağlantısı e-posta adresinize gönderildi.");
       } else {
-        setMessage('Şifre sıfırlama işlemi başarısız oldu.');
+        setMessage("Şifre sıfırlama işlemi başarısız oldu.");
       }
     } catch (error) {
-      console.error('Şifre sıfırlama işlemi hatası:', error.message);
+      console.error("Şifre sıfırlama işlemi hatası:", error.message);
     }
   };
 
   return (
-    <div className="forgot-password-container mt-5 d-flex flex-column align-items-center">
+    <div className=" container forgot-password-container mt-5  d-flex flex-column align-items-center">
       <h2 className="forgot-password-heading">Sahiden</h2>
-      <p className="forgot-password-text">E-posta adresini girerek şifre sıfırlama bağlantısı alabilirsin.</p>
+      <p className="forgot-password-text"></p>
 
-      <label htmlFor="email" className="forgot-password-label">E-posta Adresi:</label>
+      <label htmlFor="email" className="forgot-password-label"></label>
       <input
         type="email"
         id="email"
         className="forgot-password-input"
+        placeholder="E-posta giriniz"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
