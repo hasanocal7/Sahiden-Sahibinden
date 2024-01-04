9a0966d191fb94be029e7529d8dbd45c051e5466
Diyar <diyaryvzz@gmail.com>
2024-01-04 18:15:33 +0300

    car organized

--- client/src/models/Car.js
+++ client/src/models/Car.js
@@ -1,66 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from '../components/Navbar';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import axios from "axios";
+import "../App.css";
+
 function Car() {
-  const [carDetails, setCarDetails] = useState({
-    ilanNo: '',
-    ilanTarihi: '',
-    marka: '',
-    seri: '',
-    model: '',
-    yil: '',
-    yakit: '',
-    vites: '',
-    kilometre: '',
-    kasaTipi: '',
-    cekis: '',
+  const [adData, setAdData] = useState({
+    brand: "",
+    series: "",
+    model: "",
+    year: "",
+    fuel: "",
+    gear: "",
+    km: "",
+    case_type: "",
+    traction: "",
   });
-const token = localStorage.getItem("token")
+
+  const token = localStorage.getItem("token");
+
   useEffect(() => {
-    // Backend'den verileri çekmek için bir API çağrısı yapılabilir.
-    // Bu örnek için sabit bir veri kullanılmıştır. Gerçek projede bir API endpoint kullanmanız gerekecek.
     const fetchData = async () => {
       try {
-        // Örnek API çağrısı
         const response = await axios.get(
           "https://sahiden-sahibinden-production.up.railway.app/api/ads",
-          
           {
             headers: {
               Authorization: `Bearer ${token}`,
             },
           }
         );
-        const data =  response.data;
-
-        // API'den gelen verileri state'e set et
-        console.log(data)
+        const data = response.data;
+        setAdData(data);
+        console.log(data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
-
-    // fetchData fonksiyonunu çağır
+  
     fetchData();
-  }, []); // useEffect, component mount olduğunda bir kere çalışır
+  }, [token]);
 
   return (
     <>
       <Navbar />
       <div className="carDetails">
         <div className="container">
-          <ul style={{listStyle:"none"}}>
-            <li><b>İlan No:</b> {carDetails.ilanNo}</li>
-            <li><b>İlan Tarihi:</b> {carDetails.ilanTarihi}</li>
-            <li><b>Marka:</b> {carDetails.marka}</li>
-            <li><b>Seri:</b> {carDetails.seri}</li>
-            <li><b>Model:</b> {carDetails.model}</li>
-            <li><b>Yıl:</b> {carDetails.yil}</li>
-            <li><b>Yakıt:</b> {carDetails.yakit}</li>
-            <li><b>Vites:</b> {carDetails.vites}</li>
-            <li><b>Kilometre:</b> {carDetails.kilometre}</li>
-            <li><b>Kasa Tipi:</b> {carDetails.kasaTipi}</li>
-            <li><b>Çekiş:</b> {carDetails.cekis}</li>
+          <ul style={{ listStyle: "none" }}>
+            <li>
+              <b>İlan Başlığı:</b> {adData.title}
+            </li>
+            <li>
+              <b>İlan No:</b> {adData.ilan_no}
+            </li>
+            <li>
+              <b>İlan Tarihi:</b> {adData.ilan_date}
+            </li>
+            <li>
+              <b>İlan Açıklaması:</b> {adData.description}
+            </li>
+            <li>
+              <b>Marka:</b> {adData.brand}
+            </li>
+            <li>
+              <b>Seri:</b> {adData.series}
+            </li>
+            <li>
+              <b>Model:</b> {adData.model}
+            </li>
+            <li>
+              <b>Yıl:</b> {adData.year}
+            </li>
+            <li>
+              <b>Yakıt:</b> {adData.fuel}
+            </li>
+            <li>
+              <b>Vites:</b> {adData.gear}
+            </li>
+            <li>
+              <b>Kilometre:</b> {adData.km}
+            </li>
+            <li>
+              <b>Kasa Tipi:</b> {adData.case_type}
+            </li>
+            <li>
+              <b>Çekiş:</b> {adData.traction}
+            </li>
           </ul>
         </div>
       </div>
